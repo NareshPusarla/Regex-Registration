f4190288d2798f4d5667459760cda4bfffd1066c
NareshPusarla <86452410+NareshPusarla@users.noreply.github.com>
2021-07-25 18:17:16 +0530

    Uc3

--- Regex.java
+++ Regex.java
@@ -8,6 +8,7 @@ public class Regex {
 	public static void main(String[] args) {
 		String regex_FirstName = "^[A-Z][a-z]{0,6}";
 		String regex_LastName = "^[A-Z][a-z]{0,6}";
+		String regex_for_email = "^[a-zA-Z0-9+._]+@[a-zA-Z0-9.]+$";
 		
 		Pattern pattern = Pattern.compile(regex_FirstName);
 		Matcher matcher = pattern.matcher("Pusarla");
@@ -17,7 +18,9 @@ public class Regex {
 		Matcher matcher1 = pattern1.matcher("Naresh");
 		System.out.println("Last name is "+matcher1.matches());
 		
-
+		Pattern pattern2 = Pattern.compile(regex_for_email);
+		Matcher matcher2 = pattern2.matcher("naresh.pusarla@gmail.com.in");
+		System.out.println("Valid email: "+matcher2.matches());
 
 	}
 
